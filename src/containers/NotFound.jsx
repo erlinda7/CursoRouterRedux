27d5deac7b644556cfc9a9ff780a05a7f1376a5d
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-13 16:14:22 -0400

    component error 404 y ReactFragment

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const NotFound = ()=>(
+
+    <> 
+    <h1>No Encontrado</h1>
+    <h2>Regresa al Home</h2>
+    </> //reemplaza al <React.Fragment></React.Fragment>
+    //nos ahorra un div
+)
+
+export default NotFound;
