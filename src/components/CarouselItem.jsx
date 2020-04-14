92f837f4af3638463c05870a4434b940030e71e4
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-13 20:43:55 -0400

    icono eliminar solo en favoritos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -6,10 +6,10 @@ import "../assets/styles/components/CarouselItem.scss";
 import playIcon from "../assets/static/play-icon.png";
 import plusIcon from "../assets/static/plus-icon.png";
 
-import removeIcon from '../assets/static/remove-icon.png';
+import removeIcon from "../assets/static/remove-icon.png";
 
 const CarouselItem = (props) => {
-  const { id, cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration , isList} = props;
 
   const handleSetFavorite = () => {
     props.setFavorite({
@@ -36,18 +36,21 @@ const CarouselItem = (props) => {
             src={playIcon}
             alt="Play Icon"
           />
-          <img
-            className="carousel-item__details--img"
-            src={plusIcon}
-            alt="Plus Icon"
-            onClick={handleSetFavorite}
-          />
-          <img
-            className="carousel-item__details--img"
-            src={removeIcon}
-            alt="remove Icon"
-            onClick={()=>handleDeleteFavorite(id)}
-          />
+          {isList ? 
+            <img
+              className="carousel-item__details--img"
+              src={removeIcon}
+              alt="remove Icon"
+              onClick={() => handleDeleteFavorite(id)}
+            />
+           : 
+            <img
+              className="carousel-item__details--img"
+              src={plusIcon}
+              alt="Plus Icon"
+              onClick={handleSetFavorite}
+            />
+          }
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">
